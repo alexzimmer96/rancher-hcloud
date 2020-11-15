1348b73f95ad9d1db7ca0798f645fb8d83b6dc8b
Alexander Zimmermann <alexander.zimmermann96@gmail.com>
2020-11-15 10:29:22 +0100

    Added node template creation

---
+++ module-node-init/variables.tf
@@ -0,0 +1,19 @@
+variable "rancher_admin_token" {
+  type = string
+}
+
+variable "hcloud_token" {
+  type = string
+}
+
+variable "rancher_hostname" {
+  type = string
+}
+
+variable "lb_address" {
+  type = string
+}
+
+variable "hetzner_driver_id" {
+  type = string
+}

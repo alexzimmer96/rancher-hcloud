440751f57bc9fd12c900690ae0fc67affe3aa5e8
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2021-01-29 18:47:43 +0100

    Updated versions

--- module-cluster-init/main.tf
+++ module-cluster-init/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.23.0"
+      version = "1.24.0"
     }
     rke = {
       source  = "rancher/rke"
-      version = "1.1.5"
+      version = "1.1.7"
     }
   }
 }
